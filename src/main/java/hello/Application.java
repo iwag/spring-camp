0f993166b533b61bbab49554d8a63d62751fd1a8
iwag <iwagame@gmail.com>
2018-04-08 16:45:13 -0700

    add test

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,9 +1,14 @@
 package hello;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 @RestController
@@ -13,4 +18,16 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
+//    @Bean
+//    public CommandLineRunner commandLineRunner(ApplicationContext context) {
+//        return args -> {
+//            System.out.println("Let's go!");
+//
+//            String[] beans = context.getBeanDefinitionNames();
+//            Arrays.sort(beans);
+//            for (String name : beans) {
+//                System.out.println(name);
+//            }
+//        };
+//    }
 }
