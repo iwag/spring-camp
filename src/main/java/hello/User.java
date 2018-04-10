310316ddb4729979f25b39c2f2bd947b11fe1bd3
iwag <iwagame@gmail.com>
2018-04-10 00:44:34 -0700

    sprign data rest

--- src/main/java/hello/User.java
+++ src/main/java/hello/User.java
@@ -20,6 +20,30 @@ public class User {
         this.lastName = lastName;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
     @Override
     public String toString() {
         return "User{" +
--- src/main/java/hello/UserRepository.java
+++ src/main/java/hello/UserRepository.java
@@ -1,11 +1,14 @@
 package hello;
 
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
 
-public interface UserRepository extends CrudRepository<User, Long> {
+@RepositoryRestResource(collectionResourceRel = "users", path = "users")
+public interface UserRepository extends PagingAndSortingRepository<User, Long> {
 
-    List<User> findByLastName(String lastName);
+    List<User> findByLastName(@Param("name") String lastName);
 }
